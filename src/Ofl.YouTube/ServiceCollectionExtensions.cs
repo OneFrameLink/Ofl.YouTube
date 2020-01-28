e933baf99d52fadedba721b8bd20a275deea81ce
casperOne <casperOne@caspershouse.com>
2020-01-27 22:13:35 -0500

    Updating to .NET Standard 2.1.

--- src/Ofl.YouTube/ServiceCollectionExtensions.cs
+++ src/Ofl.YouTube/ServiceCollectionExtensions.cs
@@ -7,14 +7,12 @@ namespace Ofl.YouTube
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddYouTubeClient(this IServiceCollection serviceCollection)
+        public static IServiceCollection AddYouTubeClient(
+            this IServiceCollection serviceCollection
+        )
         {
             // Validate parameters.
             var sc = serviceCollection ?? throw new ArgumentNullException(nameof(serviceCollection));
-
-            // Add YouTubeUtilities.
-            // TODO: Consider removing?  Doesn't seem like we'd need multiple implementations.
-            sc = sc.AddTransient<IYouTubeUtilities, YouTubeUtilities>();
 
             // Add the YouTube client.
             sc.AddHttpClient<IYouTubeClient, YouTubeClient>()
